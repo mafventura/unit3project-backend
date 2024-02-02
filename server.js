d2d2832f4aa574430c91afa1c17a2c17101d0f65
Eyvlina <evylina5@gmail.com>
2024-02-02 17:12:57 +0000

    server.js updated

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ import express from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
+import jwt from 'jsonwebtoken'
+import { OAuth2Client } from 'google-auth-library'
+
+const client = new OAuth2Client();
 
 const app = express()
+const SECRET = process.env.SECRET
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -18,14 +23,23 @@ app.listen(port, () => {
 mongoose.connect(process.env.DATABASE_URL);
 
 const userSchema = new mongoose.Schema({
-    userEmail: {
-      type: String,
-      required: true,
-    },
-    lastLogin: {
-      type: Date,
-      required: true,
-    },
+  email: {
+  required: true,
+  unique: true,
+  type: String,
+  },
+  name: {
+  required: true,
+  type: String
+  },
+  // password: {
+  // required: false,
+  // type: String
+  // },
+  // authSource: {
+  // enum: [“self”, “google”],
+  // default: “self”
+  // }
   });
 
   const User = mongoose.model("User", userSchema);
@@ -63,3 +77,30 @@ const userSchema = new mongoose.Schema({
       res.sendStatus(200);
     }
   });
+
+  app.post("/google-auth", async (req, res) => {
+    const { credential, user_id } = req.body;
+    try {
+      const ticket = await client.verifyIdToken({
+        idToken: credential,
+        audience: user_id,
+      });
+      const payload = ticket.getPayload();
+      const { email, given_name } = payload;
+  
+      let user = await User.findOne({ email });
+      if (!user) {
+        user = await User.create({
+          email,
+          name: given_name,
+          // authSource: 'google'
+        });
+      }
+      user.save()
+      // const token = jwt.sign({ user }, SECRET);
+      res.status(200).json({ payload, token });
+    } catch (e) {
+      res.status(400).json({ error: e.message });
+    }
+  });
+  
