2cfd3ab5ed93f9d7315499b86ca2429ab27a985c
Mafalda <mafalda.ventura@live.com.pt>
2024-03-06 14:46:37 +0000

    railway deployment

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://unit3project-frontend-production.up.railway.app/",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
