b546d80e2268e1cf7982b20e72f795f51a03f367
jverdesoto <verdesoto.diseno@gmail.com>
2024-03-18 12:01:24 -0500

    Updated server.js -- Added regenerate function to avoid errors using cookie session with passport

--- server.js
+++ server.js
@@ -56,6 +56,20 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(function(request, response, next) {
+  if (request.session && !request.session.regenerate) {
+      request.session.regenerate = (cb) => {
+          cb()
+      }
+  }
+  if (request.session && !request.session.save) {
+      request.session.save = (cb) => {
+          cb()
+      }
+  }
+  next()
+})
+
 app.use(
   cors({
     origin: "https://journee.site",
